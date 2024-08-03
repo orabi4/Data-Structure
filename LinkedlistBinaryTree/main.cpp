63938e532de9d47fe8fd3a1ea1295f11feb55d35
adel <adel.error159@gmail.com>
2024-08-03 23:05:25 +0300

    update repo

---
+++ LinkedlistBinaryTree/main.cpp
@@ -0,0 +1,4 @@
+//Represention Linked// linked list mean it's is linear but the tree not linear structrue
+//when we use linked tree we know the null pointer with this foumula
+// the null pointer = Node +1
+
